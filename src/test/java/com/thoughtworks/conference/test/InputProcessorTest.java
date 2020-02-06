2975805b8a81511b246e60cc38f9b0488d23cb87
senthil-1117 <33269418+senthil-1117@users.noreply.github.com>
2020-02-06 15:18:30 +0800

    Add files via upload

--- src/test/java/com/thoughtworks/conference/test/InputProcessorTest.java
+++ src/test/java/com/thoughtworks/conference/test/InputProcessorTest.java
@@ -14,12 +14,10 @@ import com.thoughtworks.conference.processor.ScheduleProcessor;
  */
 public class InputProcessorTest {
 
-	
 	@Test
 	public void testScheduleForInvalidInputFile() {
-		ScheduleProcessor sp = new ScheduleProcessor();
 		try {
-			sp.prepareSchedule(InputProcessor.getInputEvents("src/test/resources/EventInput12345.txt"));
+			InputProcessor.getInputEvents("src/test/resources/EventInput12345.txt");
 
 		} catch (ConferenceException e) {
 			Assert.assertTrue("ConferenceException for invalid file not found",
@@ -31,9 +29,8 @@ public class InputProcessorTest {
 
 	@Test
 	public void testScheduleForInvalidTime() {
-		ScheduleProcessor sp = new ScheduleProcessor();
 		try {
-			sp.prepareSchedule(InputProcessor.getInputEvents("src/test/resources/EventInputWithInvalidTime.txt"));
+			InputProcessor.getInputEvents("src/test/resources/EventInputWithInvalidTime.txt");
 
 		} catch (ConferenceException e) {
 			Assert.assertTrue("ConferenceException for invalid time not found",
@@ -45,10 +42,8 @@ public class InputProcessorTest {
 
 	@Test
 	public void testUnfittableEvent() {
-		ScheduleProcessor sp = new ScheduleProcessor();
 		try {
-			sp.prepareSchedule(InputProcessor.getInputEvents("src/test/resources/UnfittableEvent.txt"));
-	
+			InputProcessor.getInputEvents("src/test/resources/UnfittableEvent.txt");
 		} catch (ConferenceException e) {
 			Assert.assertTrue("ConferenceException for invalid time not found",
 					e.getMessage().equals(ConferenceErrorCode.CONF_ERR_03.getErrorDesc()));
